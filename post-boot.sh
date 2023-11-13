05d7bdec67b9870683aeb182f43ee5b6cbf5b9ff
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-11-12 21:08:57 -0500

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -1,8 +1,7 @@
-#!/usr/bin/env bash
-#
-# (C) Copyright 2019, Xilinx, Inc.
-#
-#!/usr/bin/env bash
+install_mlnx_drivers() {
+    #sudo /proj/octfpga-PG0/tools/Mellanox/MLNX_OFED_LINUX-5.8-3.0.7.0-ubuntu20.04-x86_64/mlnxofedinstall --dpdk --force
+    sudo /proj/octfpga-PG0/tools/Mellanox/mlnx-en-5.8-3.0.7.0-ubuntu20.04-x86_64/install --dpdk --force
+}
 
 install_xrt() {
     echo "Download XRT installation package"
@@ -157,6 +156,7 @@ detect_cards
 install_xrt
 install_shellpkg
 verify_install
+install_mlnx_drivers
     
 if [ $? == 0 ] ; then
     echo "XRT and shell package installation successful."
