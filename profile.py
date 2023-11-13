4332b291ca34e24228df373eb95ac301ca5faf00
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2023-11-12 23:19:59 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -95,7 +95,7 @@ for i in range(params.nodeCount):
     #host_iface.component_id = "eth3"
     #host_iface.addAddress(pg.IPv4Address("192.168.40." + str(i+10), "255.255.255.0"))
 
-    lan.addInterface(host_iface)
+    #lan.addInterface(host_iface)
     
     # Optional Blockstore
     if params.tempFileSystemSize > 0 or params.tempFileSystemMax:
